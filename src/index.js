375240eec9ae7e3b23326f16bd17e03e3b63f127
LIASY032 <liasy032@mymail.unisa.edu.au>
2021-06-29 21:46:09 +0930

    finish Middleware

--- src/index.js
+++ src/index.js
@@ -10,14 +10,17 @@ import { userAdded } from "./store/user";
 const store = configureStore();
 
 store.dispatch(userAdded({ name: "user 1" }));
-store.dispatch(userAdded({ name: "user 2" }));
-store.dispatch(projectAdded({ name: "project 1" }));
+store.dispatch(function (dispatch, getState) {
+  dispatch({ type: "bugsRecieved", bugs: [1, 2, 3] });
+});
+// store.dispatch(userAdded({ name: "user 2" }));
+// store.dispatch(projectAdded({ name: "project 1" }));
 
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 3, userId: 1 }));
-store.dispatch(bugResolved({ id: 3 }));
-
-const bugs = getBugsByUser(1)(store.getState());
-console.log(bugs);
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugAssignedToUser({ bugId: 3, userId: 1 }));
+// store.dispatch(bugResolved({ id: 3 }));
+//
+// const bugs = getBugsByUser(1)(store.getState());
+// console.log(bugs);
